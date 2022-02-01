3fcb819f0a8b1fb92984386dde0ccb3b96a6f576
lbandy618 <97638884+lbandy618@users.noreply.github.com>
2022-02-01 15:59:35 -0500

    add extra challenges. included incorrect user input.

--- NumberAnalyzer/Program.cs
+++ NumberAnalyzer/Program.cs
@@ -12,37 +12,49 @@ namespace MyApp // Note: actual namespace depends on the project name.
             string userChoice = "y";
             do
             {
-            Console.WriteLine();
-            Console.WriteLine("Please enter an integer between 1 and 100.");
-            int userNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine();
+                bool gettingValidNumber = true;
+                int userNumber = 0;
+                Console.WriteLine();
+                while (gettingValidNumber)
+                {
+                    Console.WriteLine("Please enter an integer between 1 and 100.");
+                    userNumber = int.Parse(Console.ReadLine());
+                    if (userNumber > 0 && userNumber < 101)
+                    {
+                        gettingValidNumber = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect input entered. Please try again.");
+                    }
+                    Console.WriteLine();
+                }
+                // conditionals 
+                if (userNumber < 60 && !(userNumber % 2 == 0))
+                {
+                    Console.WriteLine("Thank you " + name + " " + userNumber + " is odd and less than 60.");
+                }
+                if (userNumber >= 2 && userNumber <= 24 && userNumber % 2 == 0)
+                {
+                    Console.WriteLine("Thank you " + name + " " + userNumber + " is even and less than 25.");
+                }
+                if (userNumber >= 26 && userNumber <= 60 && userNumber % 2 == 0)
+                {
+                    Console.WriteLine("Thank you " + name + " " + userNumber + " is even and between 26 and 60 inclusive.");
+                }
+                if (userNumber >= 60 && userNumber % 2 == 0)
+                {
+                    Console.WriteLine("Thank you " + name + " " + userNumber + " is even and greater than 60.");
+                }
+                if (userNumber > 60 && !(userNumber % 2 == 0))
+                {
+                    Console.WriteLine("Thank you " + name + " " + userNumber + " is odd and greater than 60.");
+                }
 
-            // conditionals 
-            if (userNumber < 60 && !(userNumber % 2 == 0))
-            {
-                Console.WriteLine("Thank you " + name + " " + userNumber + " is odd and less than 60.");
-            }
-            if (userNumber >= 2 && userNumber <= 24 && userNumber % 2 == 0)
-            {
-                Console.WriteLine("Thank you " + name + " " + userNumber + " is even and less than 25.");
-            }
-            if (userNumber >= 26 && userNumber <= 60 && userNumber % 2 == 0)
-            {
-                Console.WriteLine("Thank you " + name + " " + userNumber + " is even and between 26 and 60 inclusive.");
-            }
-            if (userNumber >= 60 && userNumber % 2 == 0)
-            {
-                Console.WriteLine("Thank you " + name + " " + userNumber + " is even and greater than 60.");
-            }
-            if (userNumber > 60 && !(userNumber % 2 == 0))
-            {
-                Console.WriteLine("Thank you " + name + " " + userNumber + " is odd and greater than 60.");
-            }
-
-            // Outro question loop
-            Console.WriteLine();    
-            Console.WriteLine("Would you like to continue? (y/n)?");
-            userChoice = Console.ReadLine();
+                // Outro question loop
+                Console.WriteLine();    
+                Console.WriteLine("Would you like to continue? (y/n)?");
+                userChoice = Console.ReadLine();
             } while (userChoice == "y");
 
 
